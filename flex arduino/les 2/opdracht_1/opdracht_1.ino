4fcad5276c9febf8511c6d346501a197183fe2ec
damian <34666@ma-web.nl>
2023-10-12 09:45:10 +0200

    update

---
+++ flex arduino/les 2/opdracht_1/opdracht_1.ino
@@ -0,0 +1,14 @@
+
+
+void setup() {
+  Serial.begin(115200);
+  for(int i = 0; i < 20; i++){
+    Serial.println("count: " + String(i));
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+
+}
